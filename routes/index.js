34177ab1b46ba2f03db9e1958d96962163b19d43
Sagar <sagarbalamwar888@gmail.com>
2023-11-21 16:36:49 +0530

    updated code with the new function like added insert post and show it also if the password is incorrect it will give error.

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const UserModel = require("./users")
 const PostModel = require("./posts")
 const localStrategy = require("passport-local")
 const passport = require("passport")
+const upload = require("./multer")
 passport.use(new localStrategy(UserModel.authenticate()))
 
 /* GET home page. */
@@ -14,11 +15,39 @@ router.get("/feed", function (req, res, next) {
   res.render("feed")
 })
 
-router.get("/profile", isLoggedIn, function (req, res, next) {
-  res.render("profile")
+router.post(
+  "/upload",
+  isLoggedIn,
+  upload.single("file"),
+  async function (req, res, next) {
+    if (!req.file) {
+      return res.status(400).send("No files were uploaded")
+    }
+    res.send("File uploaded successfully")
+    const user = await UserModel.findOne({
+      username: req.session.passport.user,
+    })
+    const post = await PostModel.create({
+      image: req.file.filename,
+      imageText: req.body.filecaption,
+      user: user._id,
+    })
+    user.posts.push(post._id)
+    await user.save()
+    res.redirect("/profile")
+  }
+)
+
+router.get("/profile", isLoggedIn, async function (req, res, next) {
+  let user = await UserModel.findOne({
+    username: req.session.passport.user,
+  }).populate("posts")
+  console.log(user)
+
+  res.render("profile", { user })
 })
 router.get("/login", function (req, res, next) {
-  res.render("login")
+  res.render("login", { error: req.flash("error") })
 })
 
 router.post("/register", function (req, res, next) {
@@ -37,6 +66,7 @@ router.post(
   passport.authenticate("local", {
     successRedirect: "/profile",
     failureRedirect: "/login",
+    failureFlash: true,
   }),
   function (req, res) {}
 )
